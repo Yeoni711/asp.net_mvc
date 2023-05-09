e1dee5f78608376638f690e8034aa52bab39ec1b
Yeoni711 <jin92214@gmail.com>
2023-05-09 23:13:33 +0900

    asp.net mvc core 프로젝트 시작

---
+++ AspnetCoreStudy/AspnetCoreStudy/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspnetCoreStudy.Models;
+
+namespace AspnetCoreStudy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var hongUser = new User
+            {
+                UserNo = 1,
+                UserName = "홍길동"
+            };
+
+            // 1번째 방식 View(model)
+            //return View(hongUser);
+
+            // 2번째 방식 ViewBag
+            //ViewBag.User = hongUser;
+            //return View();
+
+            // 3번째 방식 ViewData
+            ViewData["UserNo"] = hongUser.UserNo;
+            ViewData["UserName"] = hongUser.UserName;
+
+            return View();
+
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
